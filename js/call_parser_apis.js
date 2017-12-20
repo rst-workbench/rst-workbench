ed714b9bd85b1957b14c7ec741ead0908f8086cd
Arne Neumann <nlpbox.programming@arne.cl>
2017-12-20 17:10:48 +0100

    js: handle plain text and png API response

--- js/call_parser_apis.js
+++ js/call_parser_apis.js
@@ -9,9 +9,10 @@ $(document).ready(function () {
 
         //grab form data
         var input = $('#input-text').val()
+        var outputFormat = $('#output-format').val()
         var formData = new FormData();
         formData.append('input', input);
-        formData.append('output_format', 'original');
+        formData.append('output_format', outputFormat);
 
         console.log('input: ', input); // TODO: rm after debug
 
@@ -29,7 +30,12 @@ $(document).ready(function () {
             timeout: 600000,
             success: function (data) {
 
-                $("#results").append('<div><pre>\n' + data + '\n</pre></div>');
+                if (outputFormat == 'nltk-tree-png') {
+                    $("#results").append('<div><img alt="Embedded Image" src="data:image/png;base64,' + data + '" /></div>');
+                } else {
+                    $("#results").append('<div><pre>\n' + data + '\n</pre></div>');
+                }
+
                 console.log("SUCCESS : ", data);
                 $("#btnSubmit").prop("disabled", false);
 

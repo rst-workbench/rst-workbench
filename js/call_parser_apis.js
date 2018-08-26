ad7eda0018a687b5945672731b4160f00e6a632d
Arne Neumann <nlpbox.programming@arne.cl>
2018-08-26 23:55:55 +0200

    add documentation to call_parser_apis.js

--- js/call_parser_apis.js
+++ js/call_parser_apis.js
@@ -1,3 +1,7 @@
+// intercept clicks on the submit button of <form id='rst'> and send the form
+// content to several RST parsers, convert their output to the given format
+// and add that output to the results section of the site.
+//
 // based on: https://www.mkyong.com/jquery/jquery-ajax-submit-a-multipart-form/
 $(document).ready(function () {
 
@@ -17,14 +21,14 @@ $(document).ready(function () {
         formData.append('input', input);
         formData.append('output_format', outputFormat);
 
-        console.log('input: ', input); // TODO: rm after debug
+        console.log('input: ', input);
 
         // disable the non-js submit button temporarely
         $("#btnSubmit").prop("disabled", true);
 
         postForm("http://localhost:9001/parse", formData, "codra-service", outputFormat, onParserSuccess, onParserError);
         postForm("http://localhost:9002/parse", formData, "dplp-service", outputFormat, onParserSuccess, onParserError);
-        postForm("http://localhost:9999/parse", formData, "does-not-exist", outputFormat, onParserSuccess, onParserError);
+        // postForm("http://localhost:9999/parse", formData, "does-not-exist", outputFormat, onParserSuccess, onParserError);
 
     });
 });
@@ -48,6 +52,9 @@ function postForm(url, formData, serviceName, outputFormat, successFunc, errorFu
     });
 }
 
+// onParserSuccess is called when the RST parser returned a result.
+// The result is then given to `handleParserOutput()`
+// and the submit button of the form is re-enabled.
 function onParserSuccess(data, outputFormat, parserName) {
     // transform output and add it to results section
     handleParserOutput(data, outputFormat, parserName);
@@ -56,6 +63,9 @@ function onParserSuccess(data, outputFormat, parserName) {
     $("#btnSubmit").prop("disabled", false);
 }
 
+// onParserError is called when the RST parser returned an error.
+// The error message is then added to the results section
+// and the submit button of the form is re-enabled.
 function onParserError(e, parserName) {
     $("#results").append('<h2>' + parserName + ':</h2>\n\n');
     $("#results").append('<pre>\nParser error:\n' + JSON.stringify(e, null, 2) + '\n</pre>\n\n');
@@ -66,7 +76,7 @@ function onParserError(e, parserName) {
 
 
 // handleParserOutput takes the output of an RST parser, converts it if needed
-// and adds it to the <div id="results"> section
+// and adds it to the <div id="results"> section.
 function handleParserOutput(outputString, outputFormat, parserName) {
     $("#results").append('<h2>' + parserName + ':</h2>\n\n');
     if (outputFormat == 'nltk-tree-png') {
@@ -78,7 +88,11 @@ function handleParserOutput(outputString, outputFormat, parserName) {
     }
 }
 
-
+// addRS3toResults takes the content of an .rs3 file,
+// calls the rstweb-service to convert it into an image
+// and adds that image to the result section.
+// In addition, a button for downloading the rs3 file
+// is shown.
 function addRS3toResults(rs3String) {
     var formData = new FormData();
     formData.append('input', rs3String);
@@ -89,7 +103,7 @@ function addRS3toResults(rs3String) {
     $.ajax({
         type: "POST",
         enctype: 'multipart/form-data',
-        url: "http://localhost:9100/rs3_to_png_base64",
+        url: "http://localhost:9200/convert",
         data: formData,
         processData: false,
         contentType: false,
@@ -107,15 +121,11 @@ function addRS3toResults(rs3String) {
     });
 }
 
-// FIXME: implement addRSTWebEditOptionToResults
-//~ function addRSTWebEditOptionToResults(rs3String) {
-    //~ $("#results").append(
-//~ `<form id='editor' method='post'>
-    //~ <textarea id='input-rs3' style='display:none;'>${rs3String}</textarea>
-    //~ <input type='submit' id='RSTWebSubmitButton' value='Edit in rstweb'/>
-//~ </form>`);
-//~ }
-
+// This function adds a button to the site that let's
+// a user download a given text as a file.
+// By default, the file will be stored on the user's
+// computer under the given filename.
+//
 // source: https://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
 function download(filename, text) {
   var element = document.createElement('a');
@@ -130,6 +140,8 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
+// addRS3DownloadButton adds a button to download the given
+// rs3 file to the results section.
 function addRS3DownloadButton(rs3String) {
     $("#results").append(
 `<form onsubmit="download('result.rs3', this['text'].value)">
@@ -140,7 +152,18 @@ function addRS3DownloadButton(rs3String) {
 }
 
 
-
+// addPNGtoResults adds an image to the results section
+// given the base64 representation of that image.
 function addPNGtoResults(pngBase64) {
     $("#results").append('<div><img alt="Embedded Image" src="data:image/png;base64,' + pngBase64 + '" /></div>');
 }
+
+
+// FIXME: implement addRSTWebEditOptionToResults
+//~ function addRSTWebEditOptionToResults(rs3String) {
+    //~ $("#results").append(
+//~ `<form id='editor' method='post'>
+    //~ <textarea id='input-rs3' style='display:none;'>${rs3String}</textarea>
+    //~ <input type='submit' id='RSTWebSubmitButton' value='Edit in rstweb'/>
+//~ </form>`);
+//~ }
